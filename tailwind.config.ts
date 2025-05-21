ef93032d9348a9aaa350ec48e5deb1e5a8b9aa03
kareemshams14 <128652189+kareemshams14@users.noreply.github.com>
2025-05-21 00:58:35 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-  './src/**/*.{js,ts,jsx,tsx,mdx}',
-  './src/app/globals.css', // ✅ Add this line
-],
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/**/*.css',
+]
   theme: {
     extend: {
       colors: {
