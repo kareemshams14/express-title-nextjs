7be6ad7283f67861a6d3de84b93b1b8320d192ed
kareemshams14 <128652189+kareemshams14@users.noreply.github.com>
2025-06-04 12:53:06 -0400

    Add top padding to offset fixed navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [vehicleInfo, setVehicleInfo] = React.useState<any>(null);
 
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="min-h-screen flex flex-col pt-16">
       <Navbar />
 
       {/* Hero Section */}
